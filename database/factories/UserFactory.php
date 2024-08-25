bcddfb0b001bb7e1356d42f9e99188339fafb150
Santiago <santi1703@gmail.com>
2024-08-25 17:55:53 -0300

    - added UserSeeder - modified UserFactory and DatabaseSeeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->name(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'account' => fake()->numberBetween(1, 25) * 1000,
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
